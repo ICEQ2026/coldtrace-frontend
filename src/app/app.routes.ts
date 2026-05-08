60f56db0f8a9702a0741719bb726f498d95adbbd
Mauricio Pajes <mpajes2006@gmail.com>
2026-05-08 18:40:00 -0500

    feat(identity-access): add page not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,11 @@ import { Routes } from '@angular/router';
 const identityAccessRoutes = () =>
   import('./identity-access/presentation/identity-access.routes')
     .then(m => m.identityAccessRoutes);
+const pageNotFound = () =>
+  import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
 
 export const routes: Routes = [
   { path: 'identity-access', loadChildren: identityAccessRoutes },
-  { path: '', redirectTo: '/identity-access/sign-in', pathMatch: 'full' }
+  { path: '', redirectTo: '/identity-access/sign-in', pathMatch: 'full' },
+  { path: '**', loadComponent: pageNotFound, title: 'ColdTrace - Page Not Found' }
 ];
